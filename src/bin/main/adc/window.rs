aca0a47d2bb952ceab756e9590a0a69991aa9dcd
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2022-05-19 01:32:49 -0400

    put blackman-harris filter coefficients in ram

--- src/bin/main/adc/window.rs
+++ src/bin/main/adc/window.rs
@@ -5,6 +5,8 @@ pub fn rectangle(_data: &mut [i16; config::ADC_BUF_LEN]) {
     // rectangle window does nothing
 }
 
+// put in RAM: ~100us improvement
+#[link_section = ".data.adc::window::BLACKMAN_HARRIS"]
 static BLACKMAN_HARRIS: [u16; config::ADC_BUF_LEN] =
     include!(concat!(env!("OUT_DIR"), "/blackman_harris.rs"));
 
