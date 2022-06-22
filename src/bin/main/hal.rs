ffcb737b3c1bd20ef276af843c760822887f64c7
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2022-06-22 01:14:09 -0400

    add indicator LEDs

--- src/bin/main/hal.rs
+++ src/bin/main/hal.rs
@@ -7,8 +7,8 @@ pub mod pins {
     use stm32f1xx_hal::gpio::{Alternate, Analog, Output, Pin, PushPull, CRH, CRL};
 
     pub type A0_ADC1C0 = Pin<Analog, CRL, 'A', 0>;
-    pub type A3_ADC2C3 = Pin<Analog, CRL, 'A', 3>;
-    pub type A4_ADC2C4 = Pin<Analog, CRL, 'A', 4>;
+    pub type A1_ADC2C1 = Pin<Analog, CRL, 'A', 1>;
+    pub type A2_ADC2C2 = Pin<Analog, CRL, 'A', 2>;
     pub type A8_TIM1C1_PULSE = Pin<Alternate<PushPull>, CRH, 'A', 8>;
     pub type C13_DEBUG_LED = Pin<Output<PushPull>, CRH, 'C', 13>;
 }
