a1041bf68c9ea7692af483d144dab3f665814972
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2022-07-13 01:46:50 -0400

    use max sample value as default fake input amplitude

--- src/bin/main/config/debug.rs
+++ src/bin/main/config/debug.rs
@@ -2,10 +2,12 @@
 
 #![allow(clippy::erasing_op)]
 
+use crate::config;
+
 pub const FAKE_INPUT_DATA: bool = false;
 pub const FAKE_INPUT_CYCLES_PER_BUF: usize = 8 /* frequency = this * BUFFERS_PER_SEC */;
 pub const FAKE_INPUT_PHASE: usize = 0 * u16::MAX as usize / 4 /* phase = 2pi * this / u16::MAX */;
-pub const FAKE_INPUT_AMPLITUDE: u16 = u16::MAX / 2;
+pub const FAKE_INPUT_AMPLITUDE: u16 = config::adc::MAX_POSSIBLE_SAMPLE;
 
 pub const LOG_LAST_FEW_SAMPLES: bool = false;
 pub const LOG_LAST_N_SAMPLES: usize = 100;
